490332671059792c4c7043b2d12da25eb04cd600
Shreyashi Kumari <shreyashikumari2019@gmail.com>
2021-05-11 23:14:17 +0530

    frontend

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,8 @@
 import React , { Component } from "react";
 import fire from "./config/fire";
+import './Login.css';
+import log from './img/log.svg';
+import register from './img/register.svg';
 
 class Login extends Component{
 constructor(props)
@@ -13,6 +16,7 @@ constructor(props)
         password : ""
     }
 }
+
 login(e){
     e.preventDefault();
     fire.auth().signInWithEmailAndPassword(this.state.email,this.state.password).then((u)=>{
@@ -34,11 +38,18 @@ handleChange(e){
         [e.target.name] : e.target.value
     })
 }
+
 render()
 {
     return(
-        <div>
-            <form>
+
+        <div className="container">
+        <div className="forms-container">
+          <div className="signin-signup">
+            <form action="#" className="sign-in-form">
+              <h2 className="title">Sign in</h2>
+              <div className="input-field">
+                <i className="fas fa-user" />
                 <input
                 type="email"
                 id="email"
@@ -47,6 +58,9 @@ render()
                 onChange={this.handleChange}
                 value={this.state.email}
                 />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-lock" />
                 <input
                 name="password"
                 type= "password"
@@ -55,11 +69,102 @@ render()
                 placeholder="enter password"
                 value={this.state.password}
                 />
-                <button onClick={this.login}>Login</button>
-                <button onClick={this.signup}>Signup</button>
+              </div>
+              <button onClick={this.login} className="btn solid">Login</button>
+              <p className="social-text">Or Sign in with social platforms</p>
+              <div className="social-media">
+                <a href="#" className="social-icon">
+                  <i className="fab fa-facebook-f" />
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-twitter" />
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-google" />
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-linkedin-in" />
+                </a>
+              </div>
             </form>
-
+            <form action="#" className="sign-up-form">
+              <h2 className="title">Sign up</h2>
+              <div className="input-field">
+                <i className="fas fa-user" />
+                <input type="text" placeholder="Username" />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-envelope" />
+                <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="enter email address"
+                onChange={this.handleChange}
+                value={this.state.email}
+                />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-lock" />
+                <input
+                name="password"
+                type= "password"
+                onChange={this.handleChange}
+                id="password"
+                placeholder="enter password"
+                value={this.state.password}
+                />
+              </div>
+              <button onClick={this.signup} className="btn">Signup</button>
+              <input type="submit" className="btn" defaultValue="Sign up" />
+              <p className="social-text">Or Sign up with social platforms</p>
+              <div className="social-media">
+                <a href="#" className="social-icon">
+                  <i className="fab fa-facebook-f" />
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-twitter" />
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-google" />
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-linkedin-in" />
+                </a>
+              </div>
+            </form>
+          </div>
         </div>
+        <div className="panels-container">
+          <div className="panel left-panel">
+            <div className="content">
+              <h3>New here ?</h3>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
+                ex ratione. Aliquid!
+              </p>
+              <button className="btn transparent" id="sign-up-btn">
+                Sign up
+              </button>
+            </div>
+            <img src={log} className="image" alt="" />
+          </div>
+          <div className="panel right-panel">
+            <div className="content">
+              <h3>One of us ?</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+                laboriosam ad deleniti.
+              </p>
+              <button className="btn transparent" id="sign-in-btn">
+                Sign in
+              </button>
+            </div>
+            <img src={register} className="image" alt="" />
+          </div>
+        </div>
+      </div>
+      
     )
 }
 }
